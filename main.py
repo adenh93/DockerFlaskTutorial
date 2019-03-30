011498dcb10af800e51d40cb17069cc58921af25
Aden Herold <aden.herold1@gmail.com>
2019-03-30 14:21:14 +0930

    Implemented Alembic database migrations + configured to use batch operations for SQLite. dded baseline migration

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-from config import DevConfig 
+from sqlalchemy import MetaData
+from flask_migrate import Migrate
+from config import DevConfig
 from datetime import datetime
  
 app = Flask(__name__) 
-app.config.from_object(DevConfig) 
-db = SQLAlchemy(app)
+app.config.from_object(DevConfig)
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+db = SQLAlchemy(app, metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate(app, db)
+migrate.init_app(app, db, render_as_batch=True)
 
 class User(db.Model): 
   id = db.Column(db.Integer(), primary_key=True) 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-from main import app, db, User, Blog, Post, Comment, Tag
+from main import app, db, User, Blog, Post, Comment, Tag, migrate
 
 @app.shell_context_processor
 def make_shell_context():
-  return dict(app=app, db=db, User=User, Blog=Blog, Post=Post, Comment=Comment, Tag=Tag) 
+  return dict(app=app, db=db, User=User, Blog=Blog, Post=Post, Comment=Comment, Tag=Tag, migrate=migrate) 
 
