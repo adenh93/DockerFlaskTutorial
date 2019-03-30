727138b3fc6cebe67b39f2ab7f054b24ce19e2d6
Aden Herold <aden.herold1@gmail.com>
2019-03-30 17:32:03 +0930

    Added routes for blog application to main.py. Added templates folder to begin putting the blog application together.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from flask_migrate import Migrate
 from config import DevConfig
 from datetime import datetime
@@ -19,6 +19,67 @@ naming_convention = {
 db = SQLAlchemy(app, metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate(app, db)
 migrate.init_app(app, db, render_as_batch=True)
+
+@app.route('/')
+@app.route('/<int:page>')
+def home(page=1):
+  posts = Post.query.order_by(Post.created_date.desc()).paginate(page, app.config.get('POSTS_PER_PAGE', 10), False)
+  recent, top_tags = sidebar_data()
+  return render_template(
+    'home.html',
+    posts=posts,
+    recent=recent,
+    top_tags=top_tags
+  )
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+  post = Post.query.get_or_404(post_id)
+  recent, top_tags = sidebar_data()
+  return render_template(
+    'post.html',
+    post=post,
+    recent=recent,
+    top_tags=top_tags
+  )
+
+@app.route('/posts_by_tag/<string:tag_name>')
+def posts_by_tag(tag_name):
+  tag = Tag.query.filter_by(title=tag_name).first_or_404()
+  posts = tag.posts.order_by(Post.publish_date.desc()).all()
+  recent, top_tags = sidebar_data()
+  return render_template(
+    'tag.html',
+    tag=tag,
+    posts=posts,
+    recent=recent,
+    top_tags=top_tags
+  )
+
+@app.route('/posts_by_user/<string:username>')
+def posts_by_user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  posts = user.posts.order_by(Post.created_date.desc()).all()
+  recent, top_tags = sidebar_data()
+  return render_template(
+    'user.html',
+    user=user,
+    posts=posts,
+    recent=recent,
+    top_tags=top_tags
+  )
+
+def sidebar_data(): 
+  recent = Post.query.order_by( 
+    Post.publish_date.desc() 
+  ).limit(5).all() 
+  top_tags = db.session.query( 
+    Tag, func.count(tags.c.post_id).label('total') 
+  ).join( 
+    tags 
+  ).group_by(Tag).order_by('total DESC').limit(5).all() 
+ 
+  return recent, top_tags 
 
 class User(db.Model): 
   id = db.Column(db.Integer(), primary_key=True) 
@@ -77,3 +138,6 @@ class Tag(db.Model):
 
   def __init__(self, title):
     self.title = title
+
+if __name__ == '__main__':
+    app.run()
