3fee4ff447435c115823dd8624a86e6ed8640eb0
Aden Herold <aden.herold1@gmail.com>
2019-04-06 13:26:50 +0930

    Added 404 handler to main.py and added custom 404 error page

--- main.py
+++ main.py
@@ -166,5 +166,9 @@ def posts_by_user(username):
     top_tags=top_tags
   )
 
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run()
